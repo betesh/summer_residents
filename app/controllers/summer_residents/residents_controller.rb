d9113220747f45b1cfe47105dc5b1f5069abbe03
Isaac Betesh <iybetesh@gmail.com>
2012-10-30 13:17:45 -0400

    refactored residents_controller in preparation for #11

--- app/controllers/summer_residents/residents_controller.rb
+++ app/controllers/summer_residents/residents_controller.rb
@@ -3,28 +3,20 @@ module SummerResidents
     skip_before_filter :require_administrator_priveleges, only: [:edit, :update]
     before_filter :require_administrator_priveleges_if_different_user, only: [:edit, :update]
 
-    # GET /residents/1/edit
     def edit
       @resident = Resident.find(params[:id])
-      @type = params[:type]
-      respond_to do |format|
-        format.js { render action: params[:cancel] ? :show : :edit }
-      end
+      set_type
+      edit_but_show_if_cancelled
     end
   
     # PUT /residents/1
     # PUT /residents/1.json
     def update
       @resident = Resident.find(params[:id])
-      [:first_name, :last_name, :cell].each { |f|
-        @resident.__send__ "#{f}=", params[f]
-      }
+      assign_resident_attributes
       @resident.user.email = params[:email]
-      @type = params[:type]
-
-      respond_to do |format|
-        format.js { render action: (@resident.save ? :show : :errors) }
-      end
+      set_type
+      show_unless_errors @resident.save
     end
   
     # DELETE /residents/1
@@ -34,6 +26,28 @@ module SummerResidents
   
       respond_to do |format|
         format.js { render nothing: true }
+      end
+    end
+private
+    def set_type
+      @type = params[:type]
+    end
+
+    def assign_resident_attributes
+      [:first_name, :last_name, :cell].each { |f|
+        @resident.__send__ "#{f}=", params[f]
+      }
+    end
+
+    def edit_but_show_if_cancelled
+      respond_to do |format|
+        format.js { render action: params[:cancel] ? :show : :edit }
+      end
+    end
+
+    def show_unless_errors if_save
+      respond_to do |format|
+        format.js { render action: (if_save ? :show : :errors) }
       end
     end
   end
