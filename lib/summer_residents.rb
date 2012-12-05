868021dbb4ee4b60b104df4f39a1bb7a37b13686
Isaac Betesh <iybetesh@gmail.com>
2012-12-05 11:00:47 -0500

    fixes #17

--- lib/summer_residents.rb
+++ lib/summer_residents.rb
@@ -1,4 +1,19 @@
 require "summer_residents/engine"
 
 module SummerResidents
+  class Configuration
+    attr_accessor :families_per_row
+    def initialize
+      @families_per_row = 4
+    end
+  end
+  class << self
+    def config
+      @config ||= Configuration.new
+    end
+  end
+
+  def self.configure
+    yield self.config
+  end
 end
---
+++ test/dummy/config/initializers/summer_residents.rb
@@ -0,0 +1,3 @@
+SummerResidents.configure do |config|
+  config.families_per_row = 3
+end
