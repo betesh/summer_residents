eb1685bd06e56b3158856f4552363633ac6d6c41
Isaac Betesh <iybetesh@gmail.com>
2012-12-05 10:10:39 -0500

    implemented chage_phone_to_decimal migration

--- db/migrate/20121205150719_change_phone_to_decimal.rb
+++ db/migrate/20121205150719_change_phone_to_decimal.rb
@@ -1,7 +1,13 @@
 class ChangePhoneToDecimal < ActiveRecord::Migration
   def up
+    change_column :summer_residents_residents, :cell, :decimal, :precision => 10, :scale => 0
+    change_column :summer_residents_bungalows, :phone, :decimal, :precision => 10, :scale => 0
+    change_column :summer_residents_homes, :phone, :decimal, :precision => 10, :scale => 0
   end
 
   def down
+    change_column :summer_residents_residents, :cell, :integer
+    change_column :summer_residents_bungalows, :phone, :integer
+    change_column :summer_residents_homes, :phone, :integer
   end
 end
